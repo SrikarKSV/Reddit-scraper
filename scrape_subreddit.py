3232aaf64894587cb753334d5675d8bb3cf8a446
Srikar KSV <srikar1awesome@gmail.com>
2020-09-13 15:59:40 +0530

    Made the prototype to extract only user👤 posts and exclude ads.

--- scrape_subreddit.py
+++ scrape_subreddit.py
@@ -1,1 +1,17 @@
 from requests_html import HTMLSession
+
+url = "https://old.reddit.com/r/Python/"
+
+session = HTMLSession()
+
+r = session.get(url)
+
+post_containers = r.html.find(".thing.link")
+
+cleaned_post_containers = [
+    post_container
+    for post_container in post_containers
+    if "promoted" not in post_container.attrs["class"]
+]
+
+print(len(cleaned_post_containers))
