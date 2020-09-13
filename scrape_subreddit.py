d5bf43c59e9aa13fca9fe7b0d75d1c0d54e4cd49
Srikar KSV <srikar1awesome@gmail.com>
2020-09-13 22:00:01 +0530

    Created dictionary to parse data from the posts.

--- scrape_subreddit.py
+++ scrape_subreddit.py
@@ -1,10 +1,13 @@
 from requests_html import HTMLSession
 import re
+from pprint import pprint
 
 
 def get_post_containers(url: str) -> list:
 
-    assert bool(re.match("https://(www.)?(old\.)?reddit.com/r/\w+", url))
+    assert bool(
+        re.match("https://(www.)?(old\.)?reddit.com/r/\w+", url)
+    ), "The URL provided is not a valid subreddit URL"
 
     try:
         session = HTMLSession()
@@ -20,12 +23,24 @@ def get_post_containers(url: str) -> list:
             if "promoted" not in post_container.attrs["class"]
         ]
 
-        return cleaned_post_containers
+        posts_info = []
+        for post in cleaned_post_containers:
+            post_info = {
+                "title": post.find("a.title", first=True).text,
+                "author": post.find("a.author", first=True).text,
+                "post_link": post.find("a.comments", first=True).attrs.get("href"),
+                "votes": post.find(".score.unvoted", first=True).text,
+                "post_date": post.find(".tagline time", first=True).attrs.get("title"),
+            }
+
+            posts_info.append(post_info)
+
+        return posts_info
 
     except Exception as e:
         print("There was a problem occured: ", e)
 
 
 if __name__ == "__main__":
-    url = "https://old.reddit.com/r/PY2423534T/"
-    get_post_containers(url)
+    url = "https://old.reddit.com/r/Python/"
+    pprint(get_post_containers(url))
