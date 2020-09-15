7116597d009e26abbbf6f3d8238a7c35ff91b637
Srikar KSV <srikar1awesome@gmail.com>
2020-09-15 18:36:51 +0530

    ➕Added documentation and added support to input custom save file name.

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ import click
 @click.option("-c",
               type=click.Choice(['n', 'r', "ch", "c24", "cw", "cm",
                                  "cy", "ca", "th", "t24", "tw", "tm", "ty", "ta"], case_sensitive=False),
-              help="Choose any category(Default is hot): [n: new, r: rising, ch: Controversial posts from last hour, c24: Controversial posts from 24 hours, cw: Controversial posts from last week, cy: Controversial posts from last year, ca: Controversial posts all time, th: Top posts from last hour, t24: Top posts from last 24 hours, tw: Top posts from last week, tm: Top posts from last month, ty: Top posts from last year, ta: Top posts all time]",
+              help="Choose any category: [n: new, r: rising, ch: Controversial posts from last hour, c24: Controversial posts from 24 hours, cw: Controversial posts from last week, cy: Controversial posts from last year, ca: Controversial posts all time, th: Top posts from last hour, t24: Top posts from last 24 hours, tw: Top posts from last week, tm: Top posts from last month, ty: Top posts from last year, ta: Top posts all time]",
               default="nope")
-def main(url, no_of_pages, no_of_posts, c):
-    """Enter base URL of the subreddit
+@click.option("-o", "--output-file", type=str, default="reddit", help="Name output file(Default is 'reddit.csv')")
+def main(url, no_of_pages, no_of_posts, c, output_file):
+    """Enter base URL of the subreddit.
 
-    Choose scraping between number of pages(Each page has 27 posts) or posts.
-    Choose category(From the presented choices in the category argument) of the subreddit to scrape(Default is hot):
+    Ex: main.py -npg | -npo=<number of posts/pages> -c=<category> <URL> -o <output-file>
     """
     url = convert_categories(url, c)
     if no_of_pages:
-        print(no_of_pages)
-        paginate(url, no_of_pages=no_of_pages)
+        paginate(url, no_of_pages=no_of_pages, output_file=output_file)
     else:
-        paginate(url, no_of_posts=no_of_posts)
+        paginate(url, no_of_posts=no_of_posts, output_file=output_file)
 
 
 if __name__ == "__main__":
